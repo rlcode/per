6aa69b1a4a45ee0c3cbd1e1f4c2db6e7a42bd1c3
Chanwoong joo <jcwleo@naver.com>
2018-01-29 12:39:26 +0900

    Create cartpole_per.py

---
+++ cartpole_per.py
@@ -0,0 +1,262 @@
+import sys
+import gym
+import torch
+import pylab
+import random
+import numpy as np
+from collections import deque
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import transforms
+from SumTree import SumTree
+
+EPISODES = 700
+
+
+class DQN(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(DQN, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(state_size, 24),
+            nn.ReLU(),
+            nn.Linear(24, 24),
+            nn.ReLU(),
+            nn.Linear(24, action_size)
+        )
+
+    def forward(self, x):
+        return self.fc(x)
+
+
+# 카트폴 예제에서의 DQN 에이전트
+class DQNAgent():
+    def __init__(self, state_size, action_size):
+        self.render = False
+        self.load_model = False
+
+        # 상태와 행동의 크기 정의
+        self.state_size = state_size
+        self.action_size = action_size
+
+        # DQN 하이퍼파라미터
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+        self.memory_size = 20000
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.explore_step = 5000
+        self.epsilon_decay = (self.epsilon - self.epsilon_min) / self.explore_step
+        self.batch_size = 64
+        self.train_start = 1000
+
+        # 리플레이 메모리, 최대 크기 2000
+        # self.memory = deque(maxlen=self.memory_size)
+        self.memory = Memory(self.memory_size)
+
+        # 모델과 타깃 모델 생성
+        self.model = DQN(state_size, action_size)
+        self.model.apply(self.weights_init)
+        self.target_model = DQN(state_size, action_size)
+        self.optimizer = optim.Adam(self.model.parameters(),
+                                    lr=self.learning_rate)
+
+        # 타깃 모델 초기화
+        self.update_target_model()
+
+        if self.load_model:
+            self.model = torch.load('save_model/cartpole_dqn')
+
+    # weight xavier 초기화
+    def weights_init(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Linear') != -1:
+            torch.nn.init.xavier_uniform(m.weight)
+
+    # 타깃 모델을 모델의 가중치로 업데이트
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    # 입실론 탐욕 정책으로 행동 선택
+    def get_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            state = torch.from_numpy(state)
+            state = Variable(state).float().cpu()
+            q_value = self.model(state)
+            _, action = torch.max(q_value, 1)
+            return int(action)
+
+    # 샘플 <s, a, r, s'>을 리플레이 메모리에 저장
+    def append_sample(self, state, action, reward, next_state, done):
+        target = self.model(Variable(torch.FloatTensor(state))).data
+        old_val = target[0][action]
+        target_val = self.target_model(Variable(torch.FloatTensor(next_state))).data
+        if done:
+            target[0][action] = reward
+        else:
+            target[0][action] = reward + self.discount_factor * torch.max(target_val)
+
+        error = abs(old_val - target[0][action])
+
+        self.memory.add(error, (state, action, reward, next_state, done))
+
+    # 리플레이 메모리에서 무작위로 추출한 배치로 모델 학습
+    def train_model(self):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon -= self.epsilon_decay
+        # 메모리에서 배치 크기만큼 무작위로 샘플 추출
+        mini_batch, weights = self.memory.sample(self.batch_size)
+
+        states, next_states, actions, rewards, dones, is_weight = [], [], [], [], [], []
+
+        for i in range(self.batch_size):
+            states.extend(mini_batch[i][1][0])
+            actions.append(mini_batch[i][1][1])
+            rewards.append(mini_batch[i][1][2])
+            next_states.extend(mini_batch[i][1][3])
+            dones.append(mini_batch[i][1][4])
+            is_weight.append(weights[i])
+
+        # bool to binary
+        dones = np.array(dones).astype(int)
+
+        # 현재 상태에 대한 큐함수
+        states = torch.Tensor(states)
+        states = Variable(states).float()
+        pred = self.model(states)
+
+        # 행동을 one hot 인코딩
+        a = torch.LongTensor(actions).view(-1, 1)
+
+        one_hot_action = torch.FloatTensor(self.batch_size, self.action_size).zero_()
+        one_hot_action.scatter_(1, a, 1)
+
+        # 실제 한 행동에 대해서 업데이트 하기 위해서 큐함수를 one hot 값에 곱함
+        pred = torch.sum(pred.mul(Variable(one_hot_action)), dim=1)
+
+        # 다음 상태에 대한 최대 큐함수
+        next_states = torch.Tensor(next_states)
+        next_states = Variable(next_states).float()
+        next_pred = self.target_model(next_states).data.numpy()
+
+        target = rewards + (1 - dones) * self.discount_factor * np.max(next_pred, axis=1)
+        target = Variable(torch.FloatTensor(target))
+        errors = torch.abs(pred - target).data.numpy()
+        
+        # priority 업데이트
+        for i in range(self.batch_size):
+            idx = mini_batch[i][0]
+            self.memory.update(idx, errors[i])
+
+        self.optimizer.zero_grad()
+
+        # MSE Loss function
+        loss = ((pred - target) ** 2).mean()
+        loss.backward()
+
+        self.optimizer.step()
+
+
+class Memory:  # stored as ( s, a, r, s_ ) in SumTree
+    e = 0.01
+    a = 0.6
+    beta = 0.4
+    beta_increment_per_sampling = 0.001
+
+    def __init__(self, capacity):
+        self.tree = SumTree(capacity)
+        self.capacity = capacity
+
+    def _getPriority(self, error):
+        return (error + self.e) ** self.a
+
+    def add(self, error, sample):
+        p = self._getPriority(error)
+        self.tree.add(p, sample)
+
+    def sample(self, n):
+        batch = []
+        segment = self.tree.total() / n
+        priorities = []
+
+        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])
+
+        for i in range(n):
+            a = segment * i
+            b = segment * (i + 1)
+
+            s = random.uniform(a, b)
+            (idx, p, data) = self.tree.get(s)
+            priorities.append(p)
+            batch.append((idx, data))
+
+        sampling_probabilities = priorities / self.tree.total()
+        is_weight = np.power(self.tree.n_entries * sampling_probabilities, -self.beta)
+        is_weight /= is_weight.max()
+
+        return batch, is_weight
+
+    def update(self, idx, error):
+        p = self._getPriority(error)
+        self.tree.update(idx, p)
+
+
+if __name__ == "__main__":
+    # CartPole-v1 환경, 최대 타임스텝 수가 500
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    model = DQN(state_size, action_size)
+
+    # DQN 에이전트 생성
+    agent = DQNAgent(state_size, action_size)
+    scores, episodes = [], []
+
+    for e in range(EPISODES):
+        done = False
+        score = 0
+        # env 초기화
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+
+        while not done:
+            if agent.render:
+                env.render()
+
+            # 현재 상태로 행동을 선택
+            action = agent.get_action(state)
+            # 선택한 행동으로 환경에서 한 타임스텝 진행
+            next_state, reward, done, info = env.step(action)
+            next_state = np.reshape(next_state, [1, state_size])
+            # 에피소드가 중간에 끝나면 -100 보상
+            reward = reward if not done or score == 499 else -10
+
+            # 리플레이 메모리에 샘플 <s, a, r, s'> 저장
+            agent.append_sample(state, action, reward, next_state, done)
+            # 매 타임스텝마다 학습
+            if agent.memory.tree.n_entries >= agent.train_start:
+                agent.train_model()
+
+            score += reward
+            state = next_state
+
+            if done:
+                # 각 에피소드마다 타깃 모델을 모델의 가중치로 업데이트
+                agent.update_target_model()
+
+                score = score if score == 500 else score + 10
+                # 에피소드마다 학습 결과 출력
+                scores.append(score)
+                episodes.append(e)
+                pylab.plot(episodes, scores, 'b')
+                pylab.savefig("./save_graph/cartpole_dqn.png")
+                print("episode:", e, "  score:", score, "  memory length:",
+                      agent.memory.tree.n_entries, "  epsilon:", agent.epsilon)
+
+                # 이전 10개 에피소드의 점수 평균이 490보다 크면 학습 중단
+                if np.mean(scores[-min(10, len(scores)):]) > 490:
+                    torch.save(agent.model, "./save_model/cartpole_dqn")
+                    sys.exit()
