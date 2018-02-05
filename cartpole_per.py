8c3399e4dfe35d348759fae077cc05ec6439aa25
Chanwoong joo <jcwleo@naver.com>
2018-02-05 23:09:48 +0900

    Update cartpole_per.py

--- cartpole_per.py
+++ cartpole_per.py
@@ -10,11 +10,12 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision import transforms
-from SumTree import SumTree
+from priority_memory import Memory
 
-EPISODES = 700
+EPISODES = 500
 
-
+# approximate Q function using Neural Network
+# state is input and Q Value of each action is output of network
 class DQN(nn.Module):
     def __init__(self, state_size, action_size):
         super(DQN, self).__init__()
@@ -30,17 +31,20 @@ class DQN(nn.Module):
         return self.fc(x)
 
 
-# 카트폴 예제에서의 DQN 에이전트
+# DQN Agent for the Cartpole
+# it uses Neural Network to approximate q function
+# and prioritized experience replay memory & target q network
 class DQNAgent():
     def __init__(self, state_size, action_size):
+        # if you want to see Cartpole learning, then change to True
         self.render = False
         self.load_model = False
 
-        # 상태와 행동의 크기 정의
+        # get size of state and action
         self.state_size = state_size
         self.action_size = action_size
 
-        # DQN 하이퍼파라미터
+        # These are hyper parameters for the DQN
         self.discount_factor = 0.99
         self.learning_rate = 0.001
         self.memory_size = 20000
@@ -51,34 +55,33 @@ class DQNAgent():
         self.batch_size = 64
         self.train_start = 1000
 
-        # 리플레이 메모리, 최대 크기 2000
-        # self.memory = deque(maxlen=self.memory_size)
+        # create prioritized replay memory using SumTree
         self.memory = Memory(self.memory_size)
 
-        # 모델과 타깃 모델 생성
+        # create main model and target model
         self.model = DQN(state_size, action_size)
         self.model.apply(self.weights_init)
         self.target_model = DQN(state_size, action_size)
         self.optimizer = optim.Adam(self.model.parameters(),
                                     lr=self.learning_rate)
 
-        # 타깃 모델 초기화
+        # initialize target model
         self.update_target_model()
 
         if self.load_model:
             self.model = torch.load('save_model/cartpole_dqn')
 
-    # weight xavier 초기화
+    # weight xavier initialize
     def weights_init(self, m):
         classname = m.__class__.__name__
         if classname.find('Linear') != -1:
             torch.nn.init.xavier_uniform(m.weight)
 
-    # 타깃 모델을 모델의 가중치로 업데이트
+    # after some time interval update the target model to be same with model
     def update_target_model(self):
         self.target_model.load_state_dict(self.model.state_dict())
 
-    # 입실론 탐욕 정책으로 행동 선택
+    # get action from model using epsilon-greedy policy
     def get_action(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
@@ -89,7 +92,7 @@ class DQNAgent():
             _, action = torch.max(q_value, 1)
             return int(action)
 
-    # 샘플 <s, a, r, s'>을 리플레이 메모리에 저장
+    # save sample (error,<s,a,r,s'>) to the replay memory
     def append_sample(self, state, action, reward, next_state, done):
         target = self.model(Variable(torch.FloatTensor(state))).data
         old_val = target[0][action]
@@ -103,41 +106,37 @@ class DQNAgent():
 
         self.memory.add(error, (state, action, reward, next_state, done))
 
-    # 리플레이 메모리에서 무작위로 추출한 배치로 모델 학습
+    # pick samples from prioritized replay memory (with batch_size)
     def train_model(self):
         if self.epsilon > self.epsilon_min:
             self.epsilon -= self.epsilon_decay
-        # 메모리에서 배치 크기만큼 무작위로 샘플 추출
-        mini_batch, weights = self.memory.sample(self.batch_size)
 
-        states, next_states, actions, rewards, dones, is_weight = [], [], [], [], [], []
+        mini_batch, idxs, is_weights = self.memory.sample(self.batch_size)
+        mini_batch = np.array(mini_batch).transpose()
 
-        for i in range(self.batch_size):
-            states.extend(mini_batch[i][1][0])
-            actions.append(mini_batch[i][1][1])
-            rewards.append(mini_batch[i][1][2])
-            next_states.extend(mini_batch[i][1][3])
-            dones.append(mini_batch[i][1][4])
-            is_weight.append(weights[i])
+        states = np.vstack(mini_batch[0])
+        actions = list(mini_batch[1])
+        rewards = list(mini_batch[2])
+        next_states = np.vstack(mini_batch[3])
+        dones = mini_batch[4]
 
         # bool to binary
-        dones = np.array(dones).astype(int)
+        dones = dones.astype(int)
 
-        # 현재 상태에 대한 큐함수
+        # Q function of current state
         states = torch.Tensor(states)
         states = Variable(states).float()
         pred = self.model(states)
 
-        # 행동을 one hot 인코딩
+        # one-hot encoding
         a = torch.LongTensor(actions).view(-1, 1)
 
         one_hot_action = torch.FloatTensor(self.batch_size, self.action_size).zero_()
         one_hot_action.scatter_(1, a, 1)
 
-        # 실제 한 행동에 대해서 업데이트 하기 위해서 큐함수를 one hot 값에 곱함
         pred = torch.sum(pred.mul(Variable(one_hot_action)), dim=1)
 
-        # 다음 상태에 대한 최대 큐함수
+        # Q function of next state
         next_states = torch.Tensor(next_states)
         next_states = Variable(next_states).float()
         next_pred = self.target_model(next_states).data
@@ -145,84 +144,41 @@ class DQNAgent():
         rewards = torch.FloatTensor(rewards)
         dones = torch.FloatTensor(dones)
 
+        # Q Learning: get maximum Q value at s' from target model
         target = rewards + (1 - dones) * self.discount_factor * next_pred.max(1)[0]
         target = Variable(target)
 
         errors = torch.abs(pred - target).data.numpy()
 
-        # priority 업데이트
+        # update priority
         for i in range(self.batch_size):
-            idx = mini_batch[i][0]
+            idx = idxs[i]
             self.memory.update(idx, errors[i])
 
         self.optimizer.zero_grad()
 
         # MSE Loss function
-        loss = ((pred - target) ** 2).mean()
+        loss = F.mse_loss(pred, target)
         loss.backward()
 
+        # and train
         self.optimizer.step()
 
 
-class Memory:  # stored as ( s, a, r, s_ ) in SumTree
-    e = 0.01
-    a = 0.6
-    beta = 0.4
-    beta_increment_per_sampling = 0.001
-
-    def __init__(self, capacity):
-        self.tree = SumTree(capacity)
-        self.capacity = capacity
-
-    def _getPriority(self, error):
-        return (error + self.e) ** self.a
-
-    def add(self, error, sample):
-        p = self._getPriority(error)
-        self.tree.add(p, sample)
-
-    def sample(self, n):
-        batch = []
-        segment = self.tree.total() / n
-        priorities = []
-
-        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])
-
-        for i in range(n):
-            a = segment * i
-            b = segment * (i + 1)
-
-            s = random.uniform(a, b)
-            (idx, p, data) = self.tree.get(s)
-            priorities.append(p)
-            batch.append((idx, data))
-
-        sampling_probabilities = priorities / self.tree.total()
-        is_weight = np.power(self.tree.n_entries * sampling_probabilities, -self.beta)
-        is_weight /= is_weight.max()
-
-        return batch, is_weight
-
-    def update(self, idx, error):
-        p = self._getPriority(error)
-        self.tree.update(idx, p)
-
-
 if __name__ == "__main__":
-    # CartPole-v1 환경, 최대 타임스텝 수가 500
+    # In case of CartPole-v1, maximum length of episode is 500
     env = gym.make('CartPole-v1')
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     model = DQN(state_size, action_size)
 
-    # DQN 에이전트 생성
     agent = DQNAgent(state_size, action_size)
     scores, episodes = [], []
 
     for e in range(EPISODES):
         done = False
         score = 0
-        # env 초기화
+        
         state = env.reset()
         state = np.reshape(state, [1, state_size])
 
@@ -230,17 +186,17 @@ if __name__ == "__main__":
             if agent.render:
                 env.render()
 
-            # 현재 상태로 행동을 선택
+            # get action for the current state and go one step in environment
             action = agent.get_action(state)
-            # 선택한 행동으로 환경에서 한 타임스텝 진행
+            
             next_state, reward, done, info = env.step(action)
             next_state = np.reshape(next_state, [1, state_size])
-            # 에피소드가 중간에 끝나면 -100 보상
+            # if an action make the episode end, then gives penalty of -100
             reward = reward if not done or score == 499 else -10
 
-            # 리플레이 메모리에 샘플 <s, a, r, s'> 저장
+            # save the sample <s, a, r, s'> to the replay memory
             agent.append_sample(state, action, reward, next_state, done)
-            # 매 타임스텝마다 학습
+            # every time step do the training
             if agent.memory.tree.n_entries >= agent.train_start:
                 agent.train_model()
 
@@ -248,11 +204,11 @@ if __name__ == "__main__":
             state = next_state
 
             if done:
-                # 각 에피소드마다 타깃 모델을 모델의 가중치로 업데이트
+                # every episode update the target model to be same with model
                 agent.update_target_model()
 
+                # every episode, plot the play time
                 score = score if score == 500 else score + 10
-                # 에피소드마다 학습 결과 출력
                 scores.append(score)
                 episodes.append(e)
                 pylab.plot(episodes, scores, 'b')
@@ -260,7 +216,8 @@ if __name__ == "__main__":
                 print("episode:", e, "  score:", score, "  memory length:",
                       agent.memory.tree.n_entries, "  epsilon:", agent.epsilon)
 
-                # 이전 10개 에피소드의 점수 평균이 490보다 크면 학습 중단
+                # if the mean of scores of last 10 episode is bigger than 490
+                # stop training
                 if np.mean(scores[-min(10, len(scores)):]) > 490:
                     torch.save(agent.model, "./save_model/cartpole_dqn")
                     sys.exit()
